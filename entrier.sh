8508ce9fc4f04497af89fbdcf5d46875040689aa
心言自由 <XinYaanZyoy@gmail.com>
2020-05-09 20:48:10 +0530

    Update entrier.sh

--- entrier.sh
+++ entrier.sh
@@ -8,19 +8,20 @@ if [ -d "$DIR" ]; then
     i=0
     while read -r line; do
        printf '%d %s\n' "$i" "${line}"
-       echo "_$DIR/${${line}%.*}.md"
-       if [ -f "_$DIR/${'${line}'%.*}.md" ]; then
-           if [ -f "_$DIR/${'${line}'%.*}.md" ]; then
+       line = ${line};
+       echo "_$DIR/${line%.*}.md"
+       if [ -f "_$DIR/${line%.*}.md" ]; then
+           if [ -f "_$DIR/${line%.*}.md" ]; then
                echo "IMPOSSIBLE! it can't exist at both the places!";
            else
                echo "it was created!";
-               echo "creating "_$DIR/${'${line}'%.*}.md"";
-               echo "---$'\n'layout: caption$'\n'title: ${'${line}'%.*}$'\n'image: ${line}$'\n'permalink: 'captions/'${'${line}'%.*}---$'\n'" > "_$DIR/${'${line}'%.*}.md";
+               echo "creating "_$DIR/${line%.*}.md"";
+               echo "---$'\n'layout: caption$'\n'title: ${line%.*}$'\n'image: ${line}$'\n'permalink: 'captions/'${line%.*}---$'\n'" > "_$DIR/${line%.*}.md";
            fi
        else
-           if [ -f "_$DIR/${'${line}'%.*}.md" ]; then
+           if [ -f "_$DIR/${line%.*}.md" ]; then
                echo "it was deleted!";
-               rm -f "_$DIR/${'${line}'%.*}.md";
+               rm -f "_$DIR/${line%.*}.md";
            else
                echo "IMPOSSIBLE! it can't exist nowhere! it has to exist somewhere!";
            fi
