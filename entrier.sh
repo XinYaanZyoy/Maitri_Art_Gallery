6092340e0196cd928438fc055aa77e63bbd52547
心言自由 <XinYaanZyoy@gmail.com>
2020-05-09 19:13:51 +0530

    Update entrier.sh

--- entrier.sh
+++ entrier.sh
@@ -5,10 +5,11 @@ DIR="arts";
 status=false;
 if [ -d "$DIR" ]; then
     git log -1 --name-only --oneline > changes
-    mapfile -t lines < changes
-        for i in "${!lines[@]}"; do
-           printf '%d %s' "$i" "${lines[i]}"
-        done
+    count=0
+    while read -r line; do
+       printf '%d %s\n' "$count" "${line*//}"
+       (( count++ ))
+    done < changes
 else
     echo "$DIR doesn't exist!";
 fi
